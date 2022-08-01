9ba387848b29bdf2c85915a9f4fe72b192a1b1ea
vaishnavraju2001 <74817553+vaishnavraju2001@users.noreply.github.com>
2022-08-01 12:12:12 +0530

    Add files via upload

---
+++ controllers/users.js
@@ -0,0 +1,32 @@
+import UserSchema from "../models/UserSchema.js";
+import mongoose from "mongoose";
+export const createQuestion = (req, res) => {
+  const user = req.body;
+  const newUser = new UserSchema(user);
+
+  try {
+    newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const getQuestions = async(req,res)=>{
+    try {
+        const questions = await UserSchema.find();
+        // console.log(postMessages);
+        res.status(200).json(questions)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+export const answerQuestion= async (req,res) =>{
+    const {id: _id} = req.params
+    const post = req.body
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+   const answer = await UserSchema.findByIdAndUpdate(_id,{ ...post , _id},{new:true})
+
+   res.json(answer)
+}
